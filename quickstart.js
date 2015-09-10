71ff9063926efdfee7d8b524edf2beff90063c4a
Zulfat Ilyasov <mail@zulfat.net>
2015-09-10 17:56:08 +0300

    rebuild

--- quickstart.js
+++ quickstart.js
@@ -16,7 +16,7 @@ Treed.prototype.quickstart = function (el, options) {
   return this.initStore(options.data || {content: ''}, {
     actions: options.actions || require('./views/list/actions'),
   }).then(() => {
-
+    return this.startView(el, options)
   }).catch(error => {
     console.warn('Treed initialization failed!', error)
     throw error
@@ -25,29 +25,24 @@ Treed.prototype.quickstart = function (el, options) {
 
 Treed.prototype.startView = function (el, options) {
   options = options || {}
-  
-  var viewOptions = extend({
-    keys: options.keys || ListView.keys,
-  }, options.viewOptions)
+  return new Promise((resolve, reject) => {
+    var viewOptions = extend({
+      keys: options.keys || ListView.keys,
+    }, options.viewOptions)
+    var View = options.View || ListView
+      , props = this.addView(viewOptions)
 
-  var View = options.View || ListView
-    , props = this.addView(viewOptions)
-
-  return {
-    View: View,
-    props: props
-  };
-
-    // React.render(<View {...props}/>, el, function (err) {
-    //   if (err) return reject(err)
-    //   resolve(props.store)
-    // })
+    React.render(<View {...props}/>, el, function (err) {
+      if (err) return reject(err)
+      resolve(props.store)
+    })
+  })
 }
 
 function ensureElement(el) {
   if ('string' === typeof el) {
     var found = document.querySelector(el)
-    // if (!found) throw new Error('element not found: ' + el)
+    if (!found) throw new Error('element not found: ' + el)
     el = found
   }
   return el
